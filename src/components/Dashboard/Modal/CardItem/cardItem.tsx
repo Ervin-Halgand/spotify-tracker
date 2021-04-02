38c1fc458e104a19ccd37292b0a8330048e0464b
Halgand Ervin <halgand.ervin@gmail.com>
2021-04-02 18:08:26 +0200

    adding play logo when over(recommandation modal)

--- src/components/Dashboard/Modal/CardItem/cardItem.tsx
+++ src/components/Dashboard/Modal/CardItem/cardItem.tsx
@@ -3,7 +3,10 @@ import './style.css'
 export const CardItem = ({ data }: any) => {
     return (
         <section className="card_item" onClick={() => window.open(data.external_urls.spotify, "_blank")}>
-            <img className="card_item_img" src={data.album.images[0].url} alt={`${data.album.album_type} ${data.album.name}`} />
+            <div className="card_item_img_container">
+                <img className="card_item_img" src={data.album.images[0].url} alt={`${data.album.album_type} ${data.album.name}`} />
+                <img className="card_item_img_after" src={`${process.env.PUBLIC_URL}/images/play-logo.png`} alt="Play" />
+            </div>
             <div className="card_item_info">
                 <h1 className="card_item_info_artist_name">{data.artists[0].name}</h1>
                 <span className="card_item_info_name">
